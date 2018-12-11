c16170dfa609ada8f9cc573f196db1d240ecac4d
Rafik-Mikhail <fecolouis@gmail.com>
2018-12-11 22:31:05 +0200

    file clean-up

--- src/Try.java
+++
@@ -1,9 +0,0 @@
-import java.util.Vector;
-
-public class Try{
-    public static void main(String[]args){
-        Vector<String> c = new Vector<String>();
-        Vector<String> b = (Vector)c.clone();
-        System.out.println(b);
-    }
-}
